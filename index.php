26f2a38207cb14ec56f0a4d49aaf5aaa037e4161
israattal22-creator <israattal22@gmail.com>
2026-07-10 18:09:31 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php 
-$a = 3;
-$b = 9;
-echo $a + $b;
+
+// $age = 18;
+
+// if(&age >= 18){
+//     echo "you are eligable for voting"
+// }
+// else{
+//     echo "you are not eligable for voting"
+// }
+
+
+&perccentage =50;
+
+if(&percentage >= 90){
+    echo "you got A1";
+}else if(&percentage >= 80){
+    echo "you got A+";
+}else if(&percentage >= 70){
+    echo "you got A";
+}else if(&percentage >= 60){
+    echo "you got B";
+}else if(&percentage >= 50){
+    echo "you got C";
+}else{
+    echo "you are fail";
+}
 
 
 ?>
