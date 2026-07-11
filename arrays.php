ce11de61ba65e1be77fd684de6c077481a23d756
israattal22-creator <israattal22@gmail.com>
2026-07-11 20:54:43 +0500

    Add files via upload

---
+++ arrays.php
@@ -0,0 +1,23 @@
+<?php
+
+//simple array
+
+$numbers = [2,3,6,5,4,8];
+
+echo "<pre>";
+print_r($numbers);
+echo "<pre/>";
+
+
+//associative array
+
+$person = [
+    "usernmae" => "shabbir ghulam",
+    "age" => "24",
+    "cnic" => 1306014587544,
+    "designation" => "software engineer",
+];
+
+echo "<pre>";
+print_r($person);
+echo "<pre/>";
