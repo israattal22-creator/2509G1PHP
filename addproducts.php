45db76fba3ec8b569e10878396c6071df5ea1b14
israattal22-creator <israattal22@gmail.com>
2026-07-20 19:46:37 +0500

    Add files via upload

---
+++ addproducts.php
@@ -0,0 +1,94 @@
+<?php
+
+include("connection.php");
+
+try{
+
+   $insertquery = "INSERT INTO products (prod_name, prod_price, prod_desc)
+VALUES ('$prodname', '$prodprice', '$proddesc')";
+
+   if(isset($_POST["prodBtn"])) {
+
+    $prodname = $_POST["prodName"];
+    $prodprice = $_POST["prodPrice"];
+    $proddesc = $_POST["prodDesc"];
+}
+
+
+
+
+    } catch(\Throwable $xy) {
+
+    echo  $xy->getMessage();
+
+
+    }
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>products form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+   <div class="container">
+    <form class="row g-3" method="POST">
+
+        <div class="col-md-12">
+            <label for="inputEmail4" class="form-label">
+                Product Name
+            </label>
+            <input type="text"
+                   class="form-control"
+                   id="inputEmail4"
+                   name="prodName">
+        </div>
+
+        <div class="col-md-12">
+            <label for="inputEmail4" class="form-label">
+                Product Price
+            </label>
+            <input type="text"
+                   class="form-control"
+                   id="inputEmail4"
+                   name="prodPrice">
+        </div>
+
+        <div class="col-md-12">
+            <label for="inputEmail4" class="form-label">
+                Product Description
+            </label>
+            <input type="text"
+                   class="form-control"
+                   id="inputEmail4"
+                   name="prodDesc">
+        </div>
+
+        <div class="col-12">
+            <button type="submit"
+                    class="btn btn-primary"
+                    name="prodBtn">
+                Add Product
+            </button>
+        </div>
+
+    </form>
+</div>
+
+
+
+
+  </body>
+</html>
+
+
+
+
+
